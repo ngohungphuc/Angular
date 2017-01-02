d6f4fd0f14ca9516f0051f9e0f58292c50cd4801
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-01-02 17:12:18 +0700

    Creating other component

---
+++ first-app/src/app/other/other.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fa-other',
+  templateUrl: './other.component.html'
+})
+export class OtherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
