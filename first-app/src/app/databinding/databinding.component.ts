6f583f9b6fdee0738fcec8a14cbdc47933539db1
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-01-02 17:54:55 +0700

    String interpolation

---
+++ first-app/src/app/databinding/databinding.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-databinding',
+    templateUrl: './databinding.component.html',
+    styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent {
+    stringInterpolation = "This is stringInterpolation";
+    numberInterpolation = "This is numberInterpolation";
+}
