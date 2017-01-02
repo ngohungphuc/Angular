32457b632540437c6777c3774348ae7f7e41ca51
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-01-02 17:26:26 +0700

    Using multiple component

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { OtherComponent } from './other/other.component';
+import {AppComponent} from './app.component';
+import {OtherComponent} from './other/other.component';
+import {AnotherComponent} from './other/another.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    OtherComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        OtherComponent,
+        AnotherComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
