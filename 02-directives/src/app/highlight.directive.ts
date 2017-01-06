c0daddc055acc4016831a6726a369aa78b4b9bff
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-01-06 15:57:55 +0700

    Custom attribute directives

---
+++ 02-directives/src/app/highlight.directive.ts
@@ -0,0 +1,12 @@
+import {Directive, ElementRef, Renderer} from '@angular/core';
+
+@Directive({
+  selector: '[highlight]'
+})
+export class HighlightDirective {
+  constructor(private elementRef: ElementRef, private renderer: Renderer) {
+    //this.elementRef.nativeElement.style.backgroundColor = 'green';
+    this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
+  }
+
+}
