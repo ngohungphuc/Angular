52f6764cd5e288e5b27c807922c4294f1bfd54f0
hoangphucvu <ngohungphuc95@gmail.com>
2017-01-09 14:48:23 +0700

    Creating the Shopping List Service

--- recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,15 +1,22 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {Recipe} from "../recipe";
+import {ShoppingListService} from "../../shopping-list/shopping-list.service";
 
 @Component({
-  selector: 'rb-recipe-detail',
-  templateUrl: './recipe-detail.component.html'
+    selector: 'rb-recipe-detail',
+    templateUrl: './recipe-detail.component.html'
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() selectedRecipe : Recipe;
-  constructor() { }
+    @Input() selectedRecipe: Recipe;
 
-  ngOnInit() {
-  }
+    constructor(private sls: ShoppingListService) {
 
+    }
+
+    ngOnInit() {
+    }
+
+    onAddToShoppingList() {
+        this.sls.addItems(this.selectedRecipe.ingredients);
+    }
 }
--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
 import {Injectable} from '@angular/core';
 import {Recipe} from "./recipe";
+import {Ingredient} from "../shared/ingredient";
 
 @Injectable()
 export class RecipeService {
     private recipes: Recipe[] = [
-        new Recipe('Dummy', 'Dumny', 'http://simpleproductphotography.com/wp-content/uploads/2016/06/huf-converse-product-red-skidgrip-1.jpg', []),
+        new Recipe('Dummy', 'Dumny', 'http://simpleproductphotography.com/wp-content/uploads/2016/06/huf-converse-product-red-skidgrip-1.jpg', [
+            new Ingredient('French fries', 2),
+            new Ingredient('Pork meat', 1)
+        ]),
         new Recipe('Summer salad', 'okayfish', 'https://www.eukicks.com/wp-content/uploads/2009/11/2.jpg', [])
     ];
 
