e8635f98b9b9c5657b9c96cae71a59a16fd3c1db
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-01-10 20:35:35 +0700

    trigger navigate in code

--- 04-Routing/src/app/user/user.component.ts
+++ 04-Routing/src/app/user/user.component.ts
@@ -1,10 +1,18 @@
-import { Component, OnDestroy } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-component',
   template: `
       <h1>User Component</h1>
+      <button (click)="onNavigate()">Go home</button>
     `
 })
 export class UserComponent {
+  constructor(private router: Router) {
+  }
+
+  onNavigate() {
+    this.router.navigate(['/']);
+  }
 }
