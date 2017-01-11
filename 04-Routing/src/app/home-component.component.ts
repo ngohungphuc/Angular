25e7c3efcced19e28fab1503b6d27cd08b7d3726
hoangphucvu <ngohungphuc95@gmail.com>
2017-01-11 09:06:46 +0700

    Extracting Query Params

--- 04-Routing/src/app/home-component.component.ts
+++ 04-Routing/src/app/home-component.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home-component',
@@ -6,9 +8,24 @@ import { Component } from '@angular/core';
     <h1>
       Home Component!
     </h1>
+    <hr>
+    {{param}}
   `,
   styles: []
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy {
 
+  private subscription: Subscription;
+  param: string;
+
+  constructor(private route: ActivatedRoute) {
+    this.subscription = this.route.queryParams.subscribe(
+      queryParam => this.param = queryParam['analytics']
+    );
+  }
+
+  //prevent memory leak
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
