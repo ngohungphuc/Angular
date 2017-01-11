745360b29bf637e77dd660bd72f8ee10669f1700
hoangphucvu <ngohungphuc95@gmail.com>
2017-01-11 11:20:33 +0700

    Redirect routes

--- 04-Routing/src/app/app.routing.ts
+++ 04-Routing/src/app/app.routing.ts
@@ -1,16 +1,19 @@
 /**
  * Created by ngohungphuc on 10/01/2017.
  */
-import {Routes, RouterModule} from "@angular/router";
+import {RouterModule} from "@angular/router";
 import {HomeComponent} from "./home-component.component";
 import {UserComponent} from "./user/user.component";
 import {USER_ROUTES} from "./user/user.routes";
 
 
-const APP_ROUTES: Routes = [
-  {path: '', component: HomeComponent},
+const APP_ROUTES = [
   {path: 'user/:id', component: UserComponent},
-  {path: 'user/:id', component: UserComponent, children: USER_ROUTES}
+  {path: 'user/:id', component: UserComponent, children: USER_ROUTES},
+  {path: '', component: HomeComponent},
+  //catch all 404 routes
+  {path: '**', redirectTo: '/user/1', pathMatch: 'full'},
+
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
