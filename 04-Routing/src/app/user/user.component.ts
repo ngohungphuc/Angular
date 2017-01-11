1d63ba4786a9b84a117a5dc5862da00d2afa56a9
hoangphucvu <ngohungphuc95@gmail.com>
2017-01-11 09:45:59 +0700

    Passing Fragments and Preserving Query Params/ Fragments

--- 04-Routing/src/app/user/user.component.ts
+++ 04-Routing/src/app/user/user.component.ts
@@ -24,7 +24,7 @@ export class UserComponent implements OnDestroy {
   }
 
   onNavigate() {
-    this.router.navigate(['/'], {queryParams: {'analytics': 100}});
+    this.router.navigate(['/'], {queryParams: {'analytics': 100}, fragment: 'section1'});
   }
 
   ngOnDestroy() {
