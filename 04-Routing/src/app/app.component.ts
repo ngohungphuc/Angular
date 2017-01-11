70d97a5312f72b68f1060a424e7e603afc04e7ea
hoangphucvu <ngohungphuc95@gmail.com>
2017-01-11 11:35:20 +0700

    Styling Active Route Links

--- 04-Routing/src/app/app.component.ts
+++ 04-Routing/src/app/app.component.ts
@@ -2,7 +2,12 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styles: [`
+        .active{
+            color: red;
+        }
+    `]
 })
 export class AppComponent {
   title = 'app works!';
