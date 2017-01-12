5755182d9356224beae12e3feba324bf15dcd2d2
hoangphucvu <ngohungphuc95@gmail.com>
2017-01-12 15:03:03 +0700

    Navigating between Child Routes

--- recipe-book/src/app/recipes/recipe-list/recipe-item.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-item.component.ts
@@ -1,20 +1,14 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Recipe} from '../recipe';
 
 @Component({
   selector: 'rb-recipe-item',
   templateUrl: './recipe-item.component.html'
 })
-export class RecipeItemComponent implements OnInit {
+export class RecipeItemComponent {
   //strongly type assign Recipe class to recipe
   //use input so that orther component can use recipe
   @Input() recipe: Recipe;
-  recipeId: number;
-
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
+  @Input() recipeId: number;
 
 }
--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,29 +2,29 @@ import {Component, OnInit, EventEmitter, Output} from '@angular/core';
 import {Recipe} from '../recipe';
 import {RecipeService} from "../recipe.service";
 @Component({
-    selector: 'rb-recipe-list',
-    templateUrl: './recipe-list.component.html'
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html'
 })
 
 export class RecipeListComponent implements OnInit {
-    recipes: Recipe[] = [];
-    //call from recipes html to this event
-    //then pass to onSelected function
-    //then in recipe list html call onselect pass data receive from recipes
+  recipes: Recipe[] = [];
+  //call from recipes html to this event
+  //then pass to onSelected function
+  //then in recipe list html call onselect pass data receive from recipes
   /*@Output() recipeSelected = new EventEmitter<Recipe>();
    */
-    constructor(private recipeService: RecipeService) {
+  constructor(private recipeService: RecipeService) {
 
-    }
+  }
 
-    ngOnInit() {
-        this.recipes = this.recipeService.getRecipes();
-    }
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
-    //fire up the event
-  /*onSelected(recipe: Recipe) {
-   this.recipeSelected.emit(recipe);
-   }
+  //fire up the event
+  onSelected(recipe: Recipe) {
 
-   */
+  }
+
+
 }
