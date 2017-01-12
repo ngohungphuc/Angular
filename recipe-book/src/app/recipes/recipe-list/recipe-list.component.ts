b05c804c38d891f19c5e724b6541c09df65caea0
hoangphucvu <ngohungphuc95@gmail.com>
2017-01-12 15:53:34 +0700

    Active link

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe';
 import {RecipeService} from "../recipe.service";
 @Component({
@@ -8,22 +8,13 @@ import {RecipeService} from "../recipe.service";
 
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
-  //call from recipes html to this event
-  //then pass to onSelected function
-  //then in recipe list html call onselect pass data receive from recipes
-  /*@Output() recipeSelected = new EventEmitter<Recipe>();
-   */
+
   constructor(private recipeService: RecipeService) {
 
   }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
-  }
-
-  //fire up the event
-  onSelected(recipe: Recipe) {
-
   }
 
 
