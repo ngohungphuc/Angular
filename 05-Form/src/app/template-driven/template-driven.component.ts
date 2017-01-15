cd0c85972edbcb7067be27eb85e16d83a3e5440d
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-01-15 17:28:45 +0700

    Form State & CSS Classes (Template-Driven)

--- 05-Form/src/app/template-driven/template-driven.component.ts
+++ 05-Form/src/app/template-driven/template-driven.component.ts
@@ -3,7 +3,12 @@ import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'template-driven',
-  templateUrl: './template-driven.component.html'
+  templateUrl: './template-driven.component.html',
+  styles: [`
+    div .ng-invalid {
+        border:1px solid red;
+    }
+  `]
 })
 export class TemplateDrivenComponent {
   onSubmit(form: NgForm) {
