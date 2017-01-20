0f38c20d748e57c9c555fca9c570d3adb2289362
hoangphucvu <ngohungphuc95@gmail.com>
2017-01-20 09:30:13 +0700

    Angular 2 and frontend setup

--- Project/03-MeanTodo/routes/index.js
+++ Project/03-MeanTodo/routes/index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    res.render('index.html');
 });
 
 module.exports = router;
--- Project/03-MeanTodo/server.js
+++ Project/03-MeanTodo/server.js
@@ -11,7 +11,8 @@ var todo = require('./routes/todo');
 //view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, 'client')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
