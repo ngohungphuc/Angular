7aa966eae9e0b2ea2439c2665eb87a0a573def81
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-04-02 23:47:25 +0700

    Reactive form

--- basketball/src/app/players/registration/registration.component.ts
+++ basketball/src/app/players/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { RegistrationViewModel } from './registrationViewModel';
 import { TeamLookup } from '../../shared/lookup/team-lookup';
 import { InjuryLookupService } from '../../shared/lookup/injury-lookup.service';
@@ -15,10 +15,12 @@ export class RegistrationComponent implements OnInit {
   teamLookupList: TeamLookup[];
   injuryLookupList: string[];
   injuryFieldList: string[];
-  
+  registrationForm: FormGroup;
+
   constructor(
     private injuryLookupService: InjuryLookupService,
-    private teamLookupService: TeamLookupService
+    private teamLookupService: TeamLookupService,
+    private fb : FormBuilder
   ) { 
     this.registrationViewModel = new RegistrationViewModel();
   }
@@ -26,15 +28,8 @@ export class RegistrationComponent implements OnInit {
   ngOnInit() {
     this.teamLookupList = this.teamLookupService.getTeamsLookup();
     this.injuryLookupList = this.injuryLookupService.getInjuriesLookup();
+    this.buildForm();
     this.initialiseInjuryFieldList();
-  }
-
-  private initialiseInjuryFieldList() {
-    this.injuryFieldList = [];
-
-    for(let injury of this.injuryLookupList) {
-      this.injuryFieldList.push(injury.trim().replace(" ", "").toLowerCase());
-    }
   }
 
   addOrRemoveInjury(value: string) {
@@ -47,4 +42,102 @@ export class RegistrationComponent implements OnInit {
       this.registrationViewModel.injuries.splice(indexOfEntry, 1);
     }
   }
+
+  submit(){
+    //this.registrationForm.value return json object
+    this.registrationViewModel = this.registrationForm.value;
+    console.log(JSON.stringify(this.registrationViewModel));
+  }
+
+  private buildForm(){
+      this.registrationForm = this.fb.group({
+        'firstName':['',[Validators.required]],
+        'lastName':['',[Validators.required]],
+        'email':['',[Validators.required]],
+        'biography':['',[Validators.required]],
+        'dob':['',[Validators.required]],
+        'teamId':['',[Validators.required]],
+        'heightInFeet':['',[Validators.required]],
+        'heightInInches':['',[Validators.required]],
+        'weight':['',[Validators.required]],
+        'position':['',[Validators.required]],
+        'password':['',[Validators.required]],
+      });
+
+      this.registrationForm.valueChanges.subscribe(data=>this.onValueChanged(data));
+      this.onValueChanged();
+  }
+
+
+  private onValueChanged(data?:any){
+    if(!this.registrationForm) {return;}
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = this.registrationForm.get(field);
+
+      if (control && control.dirty && control.invalid) {
+        const messages = this.validationMessages[field];
+        
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  private initialiseInjuryFieldList() {
+    this.injuryFieldList = [];
+
+    for(let injury of this.injuryLookupList) {
+      this.injuryFieldList.push(injury.trim().replace(" ", "").toLowerCase());
+    }
+  }
+
+  private formErrors = {
+    'firstName': '',
+    'lastName': '',
+    'email': '',
+    'dob': '',
+    'teamId': '',
+    'heightInFeet': '',
+    'heightInInches': '',
+    'weight': '',
+    'position': '',
+    'password': ''
+  };
+
+  private validationMessages = {
+    'firstName': {
+      'required': 'First name is required'
+    },
+    'lastName': {
+      'required': 'Last name is required'
+    },
+    'email': {
+      'required': 'Email is required'
+    },
+    'dob': {
+      'required': 'DOB is required'
+    },
+    'teamId': {
+      'required': 'Team is required'
+    },
+    'heightInFeet': {
+      'required': 'Height in feet is required'
+    },
+    'heightInInches': {
+      'required': 'Height in inches is required'
+    },
+    'weight': {
+      'required': 'Weight in pounds is required'
+    },
+    'position': {
+      'required': 'Position is required'
+    },
+    'password': {
+      'required': 'Password is required'
+    }
+  };
 }
