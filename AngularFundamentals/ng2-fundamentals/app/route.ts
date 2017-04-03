053dcfaa5ffaec372ceec3748dc7dbdc7bc9dec0
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-04-04 00:03:45 +0700

    Lazy load module

--- AngularFundamentals/ng2-fundamentals/app/route.ts
+++ AngularFundamentals/ng2-fundamentals/app/route.ts
@@ -10,5 +10,7 @@ export const appRoutes:Routes = [
     { path:'events',component:EventsListComponent,resolve:{events:EventListResolver} },
     { path:'events/:id',component:EventDetailsComponent,canActivate:[EventRouteActivator] },
     { path: '404', component:Error404Component },
-    { path:'',redirectTo:'/events',pathMatch:'full' }
+    { path:'',redirectTo:'/events',pathMatch:'full' },
+    //lazy load user module
+    {path:'user',loadChildren:'app/user/user.module#UserModule'}
 ]
