b3a958f972c69fd91df2b5498e15b18f142e9f6a
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-04-03 23:48:07 +0700

    Active link

--- AngularFundamentals/ng2-fundamentals/app/nav/navbar.component.ts
+++ AngularFundamentals/ng2-fundamentals/app/nav/navbar.component.ts
@@ -5,7 +5,8 @@ import {Component} from '@angular/core'
 
 @Component({
     selector: 'nav-bar',
-    templateUrl: 'app/nav/navbar.component.html'
+    templateUrl: 'app/nav/navbar.component.html',
+    styles:[`li > a.active { color:#f97924}`]
 })
 export class NavBarComponent {
 
