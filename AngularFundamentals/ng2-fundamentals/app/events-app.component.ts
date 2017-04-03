a5254fd4131135ecf79e320aedb3d93557161425
ngohungphuc <ngohungphuc95@gmail.com>
2017-04-03 11:42:15 +0700

    Add navbar

--- AngularFundamentals/ng2-fundamentals/app/events-app.component.ts
+++ AngularFundamentals/ng2-fundamentals/app/events-app.component.ts
@@ -1,12 +1,14 @@
 /**
  * Created by phuc.ngo on 3/04/2017.
  */
-import { Component } from '@angular/core'
+import {Component} from '@angular/core'
 
 @Component({
-    selector:'event-app',
-    template:'<event-list></event-list>'
+    selector: 'event-app',
+    template: `
+        <nav-bar></nav-bar>
+        <event-list></event-list>`
 })
 export class EventsAppComponent {
-    
+
 }
---
+++ AngularFundamentals/ng2-fundamentals/app/nav/navbar.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by phuc.ngo on 3/04/2017.
+ */
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'nav-bar',
+    templateUrl: 'app/nav/navbar.component.html'
+})
+export class NavBarComponent {
+
+}
+
