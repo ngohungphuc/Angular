2536abd7e4d2f838eb048a8f10f9f52d77503881
ngohungphuc <ngohungphuc95@gmail.com>
2017-04-05 10:43:49 +0700

    multiple slot content

--- AngularFundamentals/ng2-fundamentals/app/common/collapsible-well.component.ts
+++ AngularFundamentals/ng2-fundamentals/app/common/collapsible-well.component.ts
@@ -1,17 +1,25 @@
 import { Component, Input } from '@angular/core';
 
+/**
+ * Select tag use to indicate where the content should be put using class or id
+ * 
+ * @export
+ * @class CollapsibleWellComponent
+ */
 @Component({
-    selector:'collapsible-well',
-    template:`<div (click)="toggleContent()" class="well pointable">
-        <h4 class="well-title">{{title}}</h4>
-        <ng-content *ngIf="visible"></ng-content>
+    selector: 'collapsible-well',
+    template: `<div (click)="toggleContent()" class="well pointable">
+        <h4>
+            <ng-content select="[well-title]"></ng-content>
+        </h4>
+        <ng-content select="[well-body]" *ngIf="visible"></ng-content>
     </div>`
 })
 export class CollapsibleWellComponent {
-    @Input() title:string
-    visible:boolean = true
+    @Input() title: string
+    visible: boolean = true
 
-    toggleContent(){
+    toggleContent() {
         this.visible = !this.visible
     }
 }
