d806517be9aa11b6b0933174a976a1b1640f764d
ngohungphuc <ngohungphuc95@gmail.com>
2017-04-06 11:31:50 +0700

    Use inject

--- AngularFundamentals/ng2-fundamentals/app/user/profile.component.ts
+++ AngularFundamentals/ng2-fundamentals/app/user/profile.component.ts
@@ -1,57 +1,78 @@
-import { AuthService } from './auth.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {AuthService} from './auth.service';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 import {
-  Component,
-  OnInit
+    Component,
+    OnInit,
+    Inject
 } from '@angular/core'
 import { Router } from "@angular/router";
+import { Toastr, TOASTR_TOKEN } from "../common/toastr.service";
 
 @Component({
-  templateUrl:'app/user/profile.component.html',
-   styles: [`
-    em {float:right; color:#E05C65; padding-left: 10px;}
-    .error input {background-color:#E3C3C5;}
-    .error ::-webkit-input-placeholder { color: #999; }
-    .error ::-moz-placeholder { color: #999; }
-    .error :-moz-placeholder { color:#999; }
-    .error :ms-input-placeholder { color: #999; }
-  `]
+    templateUrl: 'app/user/profile.component.html',
+    styles: [`
+        em {
+            float: right;
+            color: #E05C65;
+            padding-left: 10px;
+        }
+
+        .error input {
+            background-color: #E3C3C5;
+        }
+
+        .error ::-webkit-input-placeholder {
+            color: #999;
+        }
+
+        .error ::-moz-placeholder {
+            color: #999;
+        }
+
+        .error :-moz-placeholder {
+            color: #999;
+        }
+
+        .error :ms-input-placeholder {
+            color: #999;
+        }
+    `]
 })
 export class ProfileComponent implements OnInit {
-  profileForm:FormGroup
-  private firstName:FormControl
-  private lastName:FormControl
+    profileForm: FormGroup
+    private firstName: FormControl
+    private lastName: FormControl
 
-  constructor(private authService:AuthService,private router:Router){
+    constructor(private authService: AuthService, private router: Router,@Inject(TOASTR_TOKEN) private toastr: Toastr) {
 
-  }
+    }
 
-  ngOnInit(){
-    this.firstName = new FormControl(this.authService.currentUser.firstName,Validators.required)
-    this.lastName = new FormControl(this.authService.currentUser.lastName,Validators.required)
-    this.profileForm = new FormGroup({
-      firstName:this.firstName,
-      lastName:this.lastName
-    })
-  }
+    ngOnInit() {
+        this.firstName = new FormControl(this.authService.currentUser.firstName, Validators.required)
+        this.lastName = new FormControl(this.authService.currentUser.lastName, Validators.required)
+        this.profileForm = new FormGroup({
+            firstName: this.firstName,
+            lastName: this.lastName
+        })
+    }
 
-  saveProfile(formValues){
-    if(this.profileForm.valid){
-        this.authService.updateCurrentUser(formValues.firstName,formValues.lastName)
+    saveProfile(formValues) {
+        if (this.profileForm.valid) {
+            this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+            this.toastr.success('Profile saved')
+        }
+
+    }
+
+    validateFirstName() {
+        return this.firstName.valid || this.firstName.untouched
+    }
+
+    validateLastName() {
+        return this.lastName.valid || this.lastName.untouched
+    }
+
+    cancel() {
         this.router.navigate(['events'])
     }
-    
-  }
-
-   validateFirstName() {
-    return this.firstName.valid || this.firstName.untouched
-  }
-  
-  validateLastName() {
-    return this.lastName.valid || this.lastName.untouched
-  }
-
-  cancel(){
-    this.router.navigate(['events'])
-  }
 }
