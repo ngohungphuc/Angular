ad959c12950f7107c6997888f187e78726399fde
ngohungphuc <ngohungphuc95@gmail.com>
2017-04-12 09:57:30 +0700

    add jquery & modal component

--- AngularFundamentals/ng2-fundamentals/app/app.module.ts
+++ AngularFundamentals/ng2-fundamentals/app/app.module.ts
@@ -1,16 +1,16 @@
 /**
  * Created by phuc.ngo on 3/04/2017. */
-import {NgModule}from '@angular/core'
-import {BrowserModule}from '@angular/platform-browser'
-import {EventsAppComponent}from './events-app.component'
-import {NavBarComponent}from "./nav/navbar.component";
-import {TOASTR_TOKEN, Toastr}from './common/toastr.service';
-import {appRoutes}from './route';
-import {RouterModule}from '@angular/router';
-import {Error404Component}from "./errors/404.component";
-import {AuthService} from './user/auth.service';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {CollapsibleWellComponent} from "./common/collapsible-well.component";
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { EventsAppComponent } from './events-app.component'
+import { NavBarComponent } from "./nav/navbar.component";
+import { TOASTR_TOKEN, Toastr, JQ_TOKEN, CollapsibleWellComponent, SimpleModalComponent } from './common/index';
+import { appRoutes } from './route';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from "./errors/404.component";
+import { AuthService } from './user/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import {
     EventsListComponent,
     EventThumbnailComponent,
@@ -24,7 +24,8 @@ import {
     DurationPipe
 } from './events/index'
 //global object
-declare let toastr: Toastr
+declare let toastr: Toastr;
+declare let jQuery: Object;
 
 @NgModule({
     imports: [
@@ -43,18 +44,19 @@ declare let toastr: Toastr
         CreateSessionComponent,
         SessionListComponent,
         CollapsibleWellComponent,
+        SimpleModalComponent,
         DurationPipe
     ],
     providers: [EventService,
-        {provide: TOASTR_TOKEN, useValue: toastr},
+        { provide: TOASTR_TOKEN, useValue: toastr },
+        { provide: JQ_TOKEN, useValue: jQuery },
         EventRouteActivator,
         AuthService,
-        EventListResolver, {provide: 'canDeactivateCreateEvent', useValue: checkDirtyState}
+        EventListResolver, { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState }
     ],
     bootstrap: [EventsAppComponent]
 })
 export class AppModule {
-
 }
 
 function checkDirtyState(component: CreateEventComponent) {
---
+++ AngularFundamentals/ng2-fundamentals/app/common/index.ts
@@ -0,0 +1,4 @@
+export * from './jquery.service';
+export * from './toastr.service';
+export * from './collapsible-well.component';
+export * from './simple-modal.component';
