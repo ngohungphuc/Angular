7cbb93f2563d002062ac7faf262d1ce5fd205ce9
ngohungphuc <ngohungphuc95@gmail.com>
2017-09-06 22:03:40 +0700

    update code

--- redux/redux-app/src/app/app.component.ts
+++ redux/redux-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgRedux } from 'ng2-redux';
+import { NgRedux, select } from 'ng2-redux';
 import { IAppState } from './store';
 import { INCREMENT } from './action';
 @Component({
@@ -8,14 +8,13 @@ import { INCREMENT } from './action';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  counter = 0;
+  @select() counter;
 
   constructor(private ngRedux: NgRedux<IAppState>) {
-
   }
 
   increment() {
     // dispatch action
-    this.ngRedux.dispatch({type: INCREMENT});
+    this.ngRedux.dispatch({ type: INCREMENT });
   }
 }
