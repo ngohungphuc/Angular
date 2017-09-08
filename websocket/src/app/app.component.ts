1b28937ea150d7f4deeca37f66ef1972cbda3842
ngohungphuc <ngohungphuc95@gmail.com>
2017-09-08 13:12:51 +0700

    web socket sample

---
+++ websocket/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { WebsocketService } from './websocket.service'
+import { ChatService } from './chat.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [ WebsocketService, ChatService ]
+})
+export class AppComponent {
+
+	constructor(private chatService: ChatService) {
+		chatService.messages.subscribe(msg => {			
+      console.log("Response from websocket: " + msg);
+		});
+	}
+
+  private message = {
+		author: 'tutorialedge',
+		message: 'this is a test message'
+	}
+
+  sendMsg() {
+    console.log('new message from client to websocket: ', this.message);
+    console.log(this.message);
+		this.chatService.messages.next(this.message);
+		this.message.message = '';
+	}
+
+}
